2d848b2c678405560facaadd2e3a5d6af7100438
Taylor Belland <taylorbelland@gmail.com>
2019-10-31 20:21:29 -0700

    dev prod

--- main.go
+++ main.go
@@ -2,10 +2,65 @@ package main
 
 import (
   "time"
+  "strconv"
+  "strings"
   "fmt"
   zmq "github.com/pebbe/zmq4"
 
 )
+
+func hash(value string) string {
+  newVal := ""
+  for i := 0;i < len(value);i++ {
+    newVal += strconv.Itoa(int(value[i])*16+25)
+  }
+  return newVal
+}
+
+func givePubKey(servepubKey string) {
+  fmt.Println("Core login procedure started")
+  login, err := zmq.NewSocket(zmq.PUSH)
+  if err != nil {
+    panic(err)
+  }
+  defer login.Close()
+  response, err := zmq.NewSocket(zmq.PULL)
+  if err != nil {
+    panic(err)
+  }
+  defer response.Close()
+  //Preferred way to connect
+  //hostname := "tcp://snowcrashnetwork.vineyard.haus:4000"
+  hostname := "tcp://*:4001"
+//  clientname := "tcp://192.168.122.1:4001"
+  err = response.Bind(hostname)
+//  err = login.Connect(hostname)
+
+  for {
+    request, err := response.Recv(0)
+    if err != nil {
+      panic(err)
+    }
+    if strings.Split(string(request), ":")[0] == "REQUESTPUBKEY" {
+        err := login.Connect("tcp://"+strings.Split(string(request), ":")[1]+":4001")
+        if err != nil {
+          panic(err)
+        }
+        _, err = login.Send(servepubKey, 0)
+
+        if err != nil {
+          panic(err)
+        }
+    }else {
+
+      _, err := login.Send("INVALID REQUEST", 0)
+      if err != nil {
+        panic(err)
+      }
+    }
+  }
+
+}
 
 func main() {
 
@@ -22,6 +77,13 @@ func main() {
     panic(err)
   }
   defer server.Close()
+  for {
+    servekey, _, err := zmq.NewCurveKeypair()
+    if err != nil {
+      panic(err)
+    }
+    givePubKey(servekey)
+  }
   if zmq.HasCurve() {
     zmq.AuthSetVerbose(true)
     zmq.AuthStart()
@@ -29,6 +91,8 @@ func main() {
 
     clientkey, clientseckey, err := zmq.NewCurveKeypair()
     servekey, servesec, err := zmq.NewCurveKeypair()
+    //have this run as it's own thread
+//    go givePubKey(servekey)
 
     zmq.AuthCurveAdd("snowcrash.network", clientkey )
     err = client.ClientAuthCurve(servekey, clientkey, clientseckey)
